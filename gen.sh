4849b9f307f087dc0107f176b91766fc7d98e883
Arnav Rustagi <arnrustagi@gmail.com>
2022-10-03 12:18:13 +0530

    fixed colors

--- gen.sh
+++ gen.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env bash
-
-if   [[ -z $1 || -z $2 ]]
-	echo -e "\e[32mincorrect usage\e[0m"
+#! /usr/bin/env bash
+if   [[ -z $1 || -z $2 ]]; then
+	echo -e "\e[31mincorrect usage\e[0m"
 	echo -e "usage : gen [format] [filename]"
+	exit
 fi
 
 if [[ $1 -eq "--list" || $1 -eq "-l" ]]; then
 	ls ~/.gen_templates/
-else; then
+else
 	cp ~/.gen_templates/$1 ./$2
 fi
